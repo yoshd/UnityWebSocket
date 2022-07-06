8cc8d411291301ccbf01cf2933ab6054c8f4aac6
yoshd <garlic.ba.0129@gmail.com>
2022-07-07 04:11:54 +0900

    Fix SynchronizationContext switching when not WebGL

--- Assets/Runtime/Internal/WebSocketClientNonWebGL.cs
+++ Assets/Runtime/Internal/WebSocketClientNonWebGL.cs
@@ -11,35 +11,35 @@ namespace UnityWebSocket
 
         public async UniTask ConnectAsync(Uri uri, CancellationToken cancellationToken)
         {
-            await _client.ConnectAsync(uri, cancellationToken).ConfigureAwait(false);
+            await _client.ConnectAsync(uri, cancellationToken);
         }
 
         public async UniTask SendAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken)
         {
-            await _client.SendAsync(buffer, WebSocketMessageType.Binary, true, cancellationToken).ConfigureAwait(false);
+            await _client.SendAsync(buffer, WebSocketMessageType.Binary, true, cancellationToken);
         }
 
         public async UniTask SendAsync(ArraySegment<byte> buffer, CancellationToken cancellationToken)
         {
-            await _client.SendAsync(buffer, WebSocketMessageType.Binary, true, cancellationToken).ConfigureAwait(false);
+            await _client.SendAsync(buffer, WebSocketMessageType.Binary, true, cancellationToken);
         }
 
         public async UniTask<ValueWebSocketReceiveResult> ReceiveAsync(Memory<byte> buffer,
             CancellationToken cancellationToken)
         {
-            return await _client.ReceiveAsync(buffer, cancellationToken).ConfigureAwait(false);
+            return await _client.ReceiveAsync(buffer, cancellationToken);
         }
 
         public async UniTask<WebSocketReceiveResult> ReceiveAsync(ArraySegment<byte> buffer,
             CancellationToken cancellationToken)
         {
-            return await _client.ReceiveAsync(buffer, cancellationToken).ConfigureAwait(false);
+            return await _client.ReceiveAsync(buffer, cancellationToken);
         }
 
         public async UniTask CloseAsync(WebSocketCloseStatus closeStatus, string statusDescription,
             CancellationToken cancellationToken)
         {
-            await _client.CloseAsync(closeStatus, statusDescription, cancellationToken).ConfigureAwait(false);
+            await _client.CloseAsync(closeStatus, statusDescription, cancellationToken);
         }
 
         public void Dispose()
